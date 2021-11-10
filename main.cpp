fb33c55c24325172eba88b59bf11d20ab3c0fa7f
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-11-10 21:04:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-
+// stampare a video tutti i
+// numeri della serie di Fibonacci che sono minori o uguali di “n”.
 int main() {
-   return 0;
+    int PrimoNum=0, SecondoNum=0, ProssimoNum=0, n=0;
+    cin >>n;
+    PrimoNum=1;
+    SecondoNum=1;
+    cout << PrimoNum << endl << SecondoNum << endl;
+    for (int i = 2; i < n; i++) {
+        ProssimoNum= PrimoNum+SecondoNum;
+        PrimoNum= SecondoNum;
+        SecondoNum= ProssimoNum;
+        if (ProssimoNum<=n){
+        cout << ProssimoNum << endl;
+        }
+    }
+    return 0;
 }
